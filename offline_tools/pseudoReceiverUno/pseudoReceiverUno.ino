b4adff88136d0f853da63528aa039c0363dc282e
Vasu Agrawal <vasu_agrawal@yahoo.com>
2014-11-21 19:06:18 -0500

    Added fake receiver thing

---
+++ offline_tools/pseudoReceiverUno/pseudoReceiverUno.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo brake;
+Servo steer;
+
+int brakeWritePin = 2;
+int brakeReadPin = 1;
+
+int steerWritePin = 3;
+int steerReadPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  brake.attach(brakeWritePin);
+  steer.attach(steerWritePin);
+}
+
+void loop() {
+  int brakeVal = map(analogRead(brakeReadPin), 512, 1024, 0, 255);
+  brake.write(brakeVal);
+  int steerVal = map(analogRead(steerReadPin), 512, 1024, 0, 255);
+  steer.write(steerVal);
+  Serial.print(brakeVal);
+  Serial.print(" ");
+  Serial.println(steerVal);
+}
